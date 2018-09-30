4bb14f122fbe8d13077372a2a4115d6431a29f5b
Aditya Misra <amisra14@yahoo.co.uk>
2018-09-30 09:56:01 +0530

    s3_file_handler

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(name='s3_file_handler',
+                 version="0.0.1",
+                 author="Aditya Misra",
+                 author_email="amisra14@yahoo.co.uk",
+                 description="Utility for S3 files",
+                 url="https://github.com/AdityaMisra/s3_file_handler",
+                 packages=setuptools.find_packages(),
+                 classifiers=[
+                   "Programming Language :: Python :: 3 :: 2.7",
+                   "Operating System :: OS Independent",
+                 ],
+                 )
